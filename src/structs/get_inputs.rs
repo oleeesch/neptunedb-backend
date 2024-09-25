1dc79a0e7925f3b952544ca101a88e62e3f4e6da
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-25 18:16:18 +0200

    Add Search struct in get_inputs.rs
    Introduced a new `Search` struct with optional fields for subjects, stex, examiners, seasons, and years. This struct is designed to facilitate serializing and deserializing search parameters using Serde.

---
+++ src/structs/get_inputs.rs
@@ -0,0 +1,11 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Search {
+    pub subjects: Option<String>,
+    pub stex: Option<String>,
+    pub examiners: Option<String>,
+    pub seasons: Option<String>,
+    pub years: Option<String>,
+}
+
