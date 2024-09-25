7441de3cd7a8a983a21e0b57f8f049ffa3618f66
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-25 18:16:23 +0200

    Add structs for output protocols and selection identifiers
    This commit introduces the `OutputProtocol`, `SelectionIdentifier`, and `SelectionIdentifierPair` structs to `src/structs/get_outputs.rs`. These structs include derivatives for serialization and deserialization, which will facilitate consistent data interchange and processing.

---
+++ src/structs/get_outputs.rs
@@ -0,0 +1,25 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct OutputProtocol {
+    pub uuid: String,
+    pub examiners: Vec<String>,
+    pub subjects: Vec<String>,
+    pub stex: Vec<String>,
+    pub season: Vec<String>,
+    pub years: Vec<i64>
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct SelectionIdentifier {
+    pub examiners: Vec<SelectionIdentifierPair>,
+    pub subjects: Vec<SelectionIdentifierPair>,
+    pub stex: Vec<SelectionIdentifierPair>,
+    pub seasons: Vec<SelectionIdentifierPair>
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct SelectionIdentifierPair {
+    pub id: i64,
+    pub display_name: String
+}
