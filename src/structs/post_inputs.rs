2fcb3d93c34197721d08de80324af5f13f96946d
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-25 18:16:47 +0200

    Add new struct definitions for post inputs
    Introduce new structs `Protocol`, `Create`, and `ChangeAdmin` in `post_inputs.rs` with serialization and deserialization capabilities. Additionally, define an enum `CreateField` to specify different creation fields. These additions will facilitate structured input handling for post operations.

---
+++ src/structs/post_inputs.rs
@@ -0,0 +1,29 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct Protocol {
+    pub examiner_subject_ids: Vec<(i64, i64)>,
+    pub stex_id: i64,
+    pub season_id: i64,
+    pub year: i64,
+    pub text: String
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Create {
+    pub field:CreateField ,
+    pub display_name: String
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum CreateField {
+    Examiner,
+    Subject,
+    Season,
+    Stex
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ChangeAdmin {
+    pub email_addr: String
+}
